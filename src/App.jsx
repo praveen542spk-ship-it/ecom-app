7d47d83d82c6442a78b29f702d18d7a18401aea6
praveen542spk-ship-it <praveen542spk@gmail.com>
2026-06-08 18:02:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
